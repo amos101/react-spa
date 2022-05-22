b4e21bf2b876915a46b921e720a55b050528aee1
Buso Amos <64058677+amos101@users.noreply.github.com>
2022-05-22 15:05:15 +0900

    useState&useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import ColorfulMessage from "./componants/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  //    変数、　　　　関数、　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　初期値
+  const [num, setNum] = useState(0);
+  const [showFace, setShowFace] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const onFaceShow = () => {
+    setShowFace(!showFace);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        showFace || setShowFace(true);
+      } else {
+        showFace && setShowFace(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>test</h1>
-      <p style={contentStyle}>お元気ですか</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">こんにちは</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onFaceShow}>onn/off</button>
+      <p>{num}</p>
+      {showFace && <p>(^ ^)</p>}
     </>
   );
 };
